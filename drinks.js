f1f6fbeec03ef1af23ccc109d9d9d7b8ee5bcfa0
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-08-29 22:48:10 +0600

    solved free drinks problem

---
+++ drinks.js
@@ -0,0 +1,14 @@
+/*
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burger = 550;
+
+if(burger>=500){
+    console.log("Thank you for choosing our burger! As a token of appreciation for your purchase over 500 taka, we're pleased to offer you a complimentary coke. Enjoy your meal!");
+}
+else{
+    console.log("Thank you for your order! Since your total is under 500, the coke will be an additional 30 taka.");
+}
