ef52f509757d985f6e85c3cbfed4778b9ee7040f
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-08-31 23:13:32 +0600

    solved array task3 problem

---
+++ array_task3.js
@@ -0,0 +1,14 @@
+/* 
+Task-03
+---------------------------------------------------
+Create an array of books containing different book.
+
+Use the includes method to check if the array contains a javascript book.
+
+Print a message to the console indicating whether the element is present in the array or not.
+----------------------------------------------------
+*/
+
+let booksName =["The Art of War","Art of Clear Thinking","Javascript book"];
+
+console.log(booksName.includes("Javascript book"));
