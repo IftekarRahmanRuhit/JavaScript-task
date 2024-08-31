26986a35b5119f32540b81d01bb08905a4d0bac4
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-08-31 22:17:33 +0600

    solved array task1

---
+++ array_task1.js
@@ -0,0 +1,18 @@
+/* 
+Task-01
+---------------------------------------------------
+Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+----------------------------------------------------
+*/
+
+let fruits_name = ["Apple","Orange","Mango","Lemon","Cherry"];
+
+console.log(fruits_name[3]);
+
+fruits_name[2] = "Jambura";
+
+console.log(fruits_name);
