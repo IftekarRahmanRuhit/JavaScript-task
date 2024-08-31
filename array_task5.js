56bea3a7899ad625320038b29ab1c019441aaa81
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-08-31 23:39:38 +0600

    solved array_task5 problem

---
+++ array_task5.js
@@ -0,0 +1,12 @@
+/* 
+Task-05
+---------------------------------------------------
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+----------------------------------------------------
+*/
+let firstName =["Iftekar Rahman Ruhit"];
+let age =["21"];
+
+console.log(firstName.concat(age));
