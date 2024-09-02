3478661cf08b24f8540a1199b0fc4aecfd8a92d0
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-02 15:07:16 +0600

    sum of odd numbers

---
+++ while loop/sum of odd numbers.js
@@ -0,0 +1,37 @@
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+// subtask -01
+let i=81;
+let sum=0;
+
+while(i<=131){
+    sum = sum+i;
+}
+console.log("Sum of all the odd numbers from 81 to 131 :",sum);
+
+
+
+// subtask-02
+
+/*
+let i=206;
+let sum=0;
+
+while(i<=311){
+    sum = sum+i;
+}
+console.log("Sum of all the odd numbers from 206 to 311 :",sum);
+*/
