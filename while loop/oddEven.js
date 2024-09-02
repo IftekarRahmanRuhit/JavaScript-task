563b299a10c50f569f10162d9ef00e29aea9e236
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-02 14:52:07 +0600

    oddEven

---
+++ while loop/oddEven.js
@@ -0,0 +1,36 @@
+/***
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+/***
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+// Subtask-01 
+
+let i=61;
+
+while(i<=100){
+    console.log(i);
+    i+=2;
+}
+
+
+// subtask-02
+
+/*
+
+let i=78;
+
+while(i<=98){
+    console.log(i);
+    i+=2;
+}
+
+*/
