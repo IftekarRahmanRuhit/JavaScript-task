eaa672a8b6b5b4fba3b51b4f834470b4d7aaf584
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-02 15:14:05 +0600

    multiplication table

---
+++ while loop/multiplication table.js
@@ -0,0 +1,15 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+let number=5;
+let multiplication = 1;
+let i=1;
+while(i<=10){
+    multiplication = number*i;
+    console.log(`${number} x ${i} = ${multiplication}`);
+    i++;
+}
+
