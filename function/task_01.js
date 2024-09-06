29fa5fd1b2e74065bdbbc50e9900fc822030758f
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-06 10:50:39 +0600

    task_01

---
+++ function/task_01.js
@@ -0,0 +1,13 @@
+/*
+ Task-1
+
+ Take four parameters. Multiply the four numbers and then return the result
+
+ */
+
+ function multiplication (num1,num2,num3,num4) {
+    const result = num1 * num2 * num3 * num4;
+    return result;
+ }
+ const output = multiplication(5,3,8,9);
+ console.log(output);
